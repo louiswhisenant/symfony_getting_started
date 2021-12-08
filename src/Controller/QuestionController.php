91fceb60787a6b8a718a042e527e568b6506521a
Louis Whisenant <louiswhisenant@gmail.com>
2021-12-07 22:00:06 -0600

    initial commit

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class QuestionController
+{
+    /**
+     * @Route("/")
+     */
+
+    public function homepage()
+    {
+        return new Response('What a bewitching controller we have conjured!');
+    }
+
+    /**
+     * @Route("questions/{slug}")
+     */
+
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the "%s"!',
+            ucwords(str_replace('-', ' ', $slug))
+        ));
+    }
+}
